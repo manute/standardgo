c8b32a0b970d31baea85e00270ca6d8c11e3d7d8
malonso <manuel.alonso@corp.ovh.com>
2016-09-07 12:07:36 +0200

    removing unused interface

--- http/account.go
+++ http/account.go
@@ -6,19 +6,15 @@ import (
 	"github.com/kataras/iris"
 )
 
-type AccountRestService interface {
-	List(c *iris.Context)
-}
-
-type accountService struct {
+type AccountRestService struct {
 	DomainService domain.AccountService
 }
 
-func NewAccountRestService(repo domain.AccountService) AccountRestService {
-	return &accountService{repo}
+func NewAccountRestService(repo domain.AccountService) *AccountRestService {
+	return &AccountRestService{repo}
 }
 
-func (a *accountService) List(c *iris.Context) {
+func (a *AccountRestService) List(c *iris.Context) {
 	res, err := a.DomainService.Find()
 	if err != nil {
 		c.JSON(iris.StatusOK, err)
